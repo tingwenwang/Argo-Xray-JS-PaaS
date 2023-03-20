017be553c368c516494140c02479f599f597785f
yazhuy <85384621+yazhuy@users.noreply.github.com>
2023-03-20 09:31:09 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # 设置各变量
 WSPATH=${WSPATH:-'argo'}  # WS 路径前缀。(注意:伪装路径不需要 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+UUID=${UUID:-'079bcbe3-d951-49f2-b6fc-08f579e1da8d'}
 
 # 哪吒三个参数，不需要的话可以留空，删除或在这三行最前面加 # 以注释
 # NEZHA_SERVER=${NEZHA_SERVER}
